3a4ed740a3689f8810b51a5d6616050c61c80ba0
Tungdji <wtd53pm@outlook.com>
2020-12-18 08:37:48 +0800

    Create packages 'admin', 'login', 'signup'

---
+++ src/login/LogIn.java
@@ -0,0 +1,17 @@
+package login;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.sql.*;
+
+public class LogIn extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
