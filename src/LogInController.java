a6b4464d3b0920379db0c6e444bf0b9c432b8ce3
Tungdji <wtd53pm@outlook.com>
2020-12-18 23:35:20 +0800

    Update LogInController.java

--- src/LogInController.java
+++ src/LogInController.java
@@ -1,2 +1,38 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LogInController {
+
+    @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button logInButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private TextField nameTf;
+
+    @FXML
+    private TextField passwordTf;
+
+    @FXML
+    void clearClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logInClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signInClick(ActionEvent event) {
+
+    }
+
 }
