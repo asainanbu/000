47f455236fc9fd00cef327dd7e985efacdd5c551
Tungdji <wtd53pm@outlook.com>
2020-12-19 14:36:56 +0800

    Update UserHomepageController.java

--- src/UserHomepageController.java
+++ src/UserHomepageController.java
@@ -1,9 +1,17 @@
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class UserHomepageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
 
     @FXML
     private Button notificationButton;
@@ -40,4 +48,13 @@ public class UserHomepageController {
 
     }
 
+    @FXML
+    void initialize() {
+        assert notificationButton != null : "fx:id=\"notificationButton\" was not injected: check your FXML file 'UserHomepageUI.fxml'.";
+        assert gymReservationButton != null : "fx:id=\"gymReservationButton\" was not injected: check your FXML file 'UserHomepageUI.fxml'.";
+        assert myReservationButton != null : "fx:id=\"myReservationButton\" was not injected: check your FXML file 'UserHomepageUI.fxml'.";
+        assert logOutButton != null : "fx:id=\"logOutButton\" was not injected: check your FXML file 'UserHomepageUI.fxml'.";
+        assert currentUsernameLabel != null : "fx:id=\"currentUsernameLabel\" was not injected: check your FXML file 'UserHomepageUI.fxml'.";
+
+    }
 }
