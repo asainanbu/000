aca90ef39238851a08e98c68648fb1825c986b4f
Tungdji <wtd53pm@outlook.com>
2020-12-19 16:52:28 +0800

    Update UserNotification.java

--- src/UserNotification.java
+++ src/UserNotification.java
@@ -9,6 +9,7 @@ public class UserNotification extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("UserNotificationUI.fxml"));
+        primaryStage.setTitle("消息通知");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
