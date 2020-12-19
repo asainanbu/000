10ec41acab4c29f00452d5639a97d9b1e554d92a
Tungdji <wtd53pm@outlook.com>
2020-12-19 13:09:55 +0800

    Update SignUpController.java

--- src/SignUpController.java
+++ src/SignUpController.java
@@ -1,2 +1,34 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class SignUpController {
+
+    @FXML
+    private TextField usernameTf;
+
+    @FXML
+    private Button affirmButton;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField passwordAgainField;
+
+    @FXML
+    void affirmClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnClick(ActionEvent event) {
+
+    }
+
 }
