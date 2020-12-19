6de26d3311955ace94c2e0e14fb9ae912450a608
Tungdji <wtd53pm@outlook.com>
2020-12-19 16:15:26 +0800

    Create 'AdminNotification' part

---
+++ src/AdminNotificationController.java
@@ -0,0 +1,47 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class AdminNotificationController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button returnButton;
+
+    @FXML
+    private Button releaseButton;
+
+    @FXML
+    private TextField notificationTf;
+
+    @FXML
+    private TableView<?> notificationTable;
+
+    @FXML
+    void releaseClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert returnButton != null : "fx:id=\"returnButton\" was not injected: check your FXML file 'AdminNotificationUI.fxml'.";
+        assert releaseButton != null : "fx:id=\"releaseButton\" was not injected: check your FXML file 'AdminNotificationUI.fxml'.";
+        assert notificationTf != null : "fx:id=\"notificationTf\" was not injected: check your FXML file 'AdminNotificationUI.fxml'.";
+        assert notificationTable != null : "fx:id=\"notificationTable\" was not injected: check your FXML file 'AdminNotificationUI.fxml'.";
+
+    }
+}
