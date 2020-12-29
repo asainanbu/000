e949e8d075bb7a58bc889bf41d791ba37ac45868
Tungdji <wtd53pm@outlook.com>
2020-12-29 16:08:44 +0800

    Update UserHomepage.java

--- src/UserHomepage.java
+++ src/UserHomepage.java
@@ -2,12 +2,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class UserHomepage extends GridPane {
+public class UserHomepage extends BorderPane {
 
     public UserHomepage(Stage stage) {
         try {
