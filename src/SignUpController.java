1b07db2ba78d2d838de24554449b49846b508ffd
Tungdji <wtd53pm@outlook.com>
2020-12-29 12:28:07 +0800

    Update SignUpController.java

--- src/SignUpController.java
+++ src/SignUpController.java
@@ -46,25 +46,22 @@ public class SignUpController {
         stage.show();
 
         // 关闭舞台时，会弹出模态对话框确认是否退出
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                // 对话框 Alert Alert.AlertType.CONFIRMATION
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                // 设置对话框标题
-                alert.setTitle("退出");
-                // 设置内容
-                alert.setHeaderText("确定要退出吗？");
-                // 显示对话框
-                Optional<ButtonType> result = alert.showAndWait();
-                // 如果点击OK
-                if (result.get() == ButtonType.OK) {
-                    // 关闭窗体
-                    stage.close();
-                    // 否则
-                } else {
-                    event.consume();
-                }
+        stage.setOnCloseRequest(event1 -> {
+            // 对话框 Alert Alert.AlertType.CONFIRMATION
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            // 设置对话框标题
+            alert.setTitle("退出");
+            // 设置内容
+            alert.setHeaderText("确定要退出吗？");
+            // 显示对话框
+            Optional<ButtonType> result = alert.showAndWait();
+            // 如果点击OK
+            if (result.get() == ButtonType.OK) {
+                // 关闭窗体
+                stage.close();
+                // 否则
+            } else {
+                event1.consume();
             }
         });
 
