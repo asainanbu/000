c4f5ac0693d87ef932a2974542e42db57d1fae66
Tungdji <wtd53pm@outlook.com>
2020-12-29 16:08:40 +0800

    Update UserGymReservation.java

--- src/UserGymReservation.java
+++ src/UserGymReservation.java
@@ -2,12 +2,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class UserGymReservation extends GridPane {
+public class UserGymReservation extends BorderPane {
 
 
     public UserGymReservation(Stage stage) {
@@ -17,8 +18,6 @@ public class UserGymReservation extends GridPane {
 
             // 获得控制器对象,并把数据传给控制器对象
             ((UserGymReservationController) fxmlLoader.getController()).setOldStage(stage);
-            ;
-
         } catch (IOException e) {
             e.printStackTrace();
         }
