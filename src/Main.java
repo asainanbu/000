578dd2770279143a33f358e39c0269cb9bc35aa9
Tungdji <wtd53pm@outlook.com>
2020-12-29 12:29:04 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 import java.util.Optional;
+import java.sql.*;
 
 public class Main extends Application {
 
-
+    static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/hippo_reservation?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
+    static Connection connection = null;
+    static Statement statement = null;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -21,30 +21,34 @@ public class Main extends Application {
         primaryStage.show();
 
         // 关闭舞台时，会弹出模态对话框确认是否退出
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
-                // 对话框 Alert Alert.AlertType.CONFIRMATION
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                // 设置对话框标题
-                alert.setTitle("退出");
-                // 设置内容
-                alert.setHeaderText("确定要退出吗？");
-                // 显示对话框
-                Optional<ButtonType> result = alert.showAndWait();
-                // 如果点击OK
-                if (result.get() == ButtonType.OK) {
-                    // 关闭窗体
-                    primaryStage.close();
-                    // 否则
-                } else {
-                    event.consume();
-                }
+        primaryStage.setOnCloseRequest(event -> {
+            // 对话框 Alert Alert.AlertType.CONFIRMATION
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            // 设置对话框标题
+            alert.setTitle("退出");
+            // 设置内容
+            alert.setHeaderText("确定要退出吗？");
+            // 显示对话框
+            Optional<ButtonType> result = alert.showAndWait();
+            // 如果点击OK
+            if (result.get() == ButtonType.OK) {
+                // 关闭窗体
+                primaryStage.close();
+                // 否则
+            } else {
+                event.consume();
             }
         });
     }
 
     public static void main(String[] args) {
+        try{
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DB_URL,"root","");
+            statement = connection.createStatement();
+        }catch(SQLException | ClassNotFoundException se) {
+            se.printStackTrace();
+        }
         launch(args);
     }
 }
