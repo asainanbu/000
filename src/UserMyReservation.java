7218ff35b12e7cf1ef15035c1d8718d092be15ef
Tungdji <wtd53pm@outlook.com>
2020-12-29 16:08:46 +0800

    Update UserMyReservation.java

--- src/UserMyReservation.java
+++ src/UserMyReservation.java
@@ -2,12 +2,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderRepeat;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class UserMyReservation extends GridPane {
+public class UserMyReservation extends BorderPane {
 
     public UserMyReservation(Stage stage) {
         try {
