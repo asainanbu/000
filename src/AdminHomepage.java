3926a9c7e49cbe1a75c147e89ada69de9efbffe4
Tungdji <wtd53pm@outlook.com>
2020-12-29 16:05:21 +0800

    Update AdminHomepage.java

--- src/AdminHomepage.java
+++ src/AdminHomepage.java
@@ -7,21 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-//public class AdminHomepage extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("AdminHomepageUI.fxml"));
-//        primaryStage.setTitle("主页");
-//        primaryStage.setScene(new Scene(root));
-//        primaryStage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
-
 public class AdminHomepage extends BorderPane {
 
     public AdminHomepage(Stage stage) {
