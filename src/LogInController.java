131be473b7d463e4ed71f5aa1866a502049718f0
Tungdji <wtd53pm@outlook.com>
2020-12-29 20:13:25 +0800

    Update LogInController.java

--- src/LogInController.java
+++ src/LogInController.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Currency;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -52,7 +53,7 @@ public class LogInController {
             while(memberRS.next()) {
                 if (Objects.equals(usernameTf.getText(), memberRS.getString("member_name"))) {
                     foundMember = true;
-                    if (Objects.equals(Cryptography.encrypt(usernameTf.getText()), memberRS.getString("password"))) {
+                    if (Objects.equals(Cryptography.encrypt(passwordField.getText()), memberRS.getString("password"))) {
                         correctPassword = true;
                         Main.currentMemberName = memberRS.getString("member_name");
                         break;
@@ -74,32 +75,66 @@ public class LogInController {
                 alert.showAndWait();
             }
             else if (correctPassword) {
-                Stage stage = new Stage();
-                Scene scene = new Scene(new UserHomepage(stage));
-                stage.setScene(scene);
-                stage.setTitle("用户主页");
-                stage.show();
-                // 关闭舞台时，会弹出模态对话框确认是否退出
-                stage.setOnCloseRequest(event1 -> {
-                    // 对话框 Alert Alert.AlertType.CONFIRMATION
-                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                    // 设置对话框标题
-                    alert.setTitle("退出");
-                    // 设置内容
-                    alert.setHeaderText("确定要退出吗？");
-                    // 显示对话框
-                    Optional<ButtonType> result = alert.showAndWait();
-                    // 如果点击OK
-                    if (result.get() == ButtonType.OK) {
-                        // 关闭窗体
-                        stage.close();
-                        // 否则
-                    } else {
-                        event1.consume();
-                    }
-                });
-                // 隐藏之前的窗体
-                oldStage.hide();
+                if (Objects.equals(memberRS.getString("identity"), "user")) {
+                    Main.currentMemberName = usernameTf.getText();
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(new UserHomepage(stage));
+                    stage.setScene(scene);
+                    stage.setTitle("用户主页");
+                    stage.show();
+                    // 关闭舞台时，会弹出模态对话框确认是否退出
+                    stage.setOnCloseRequest(event1 -> {
+                        // 对话框 Alert Alert.AlertType.CONFIRMATION
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        // 设置对话框标题
+                        alert.setTitle("退出");
+                        // 设置内容
+                        alert.setHeaderText("确定要退出吗？");
+                        // 显示对话框
+                        Optional<ButtonType> result = alert.showAndWait();
+                        // 如果点击OK
+                        if (result.get() == ButtonType.OK) {
+                            // 关闭窗体
+                            stage.close();
+                            // 否则
+                        } else {
+                            event1.consume();
+                        }
+                    });
+                    // 隐藏之前的窗体
+                    oldStage.hide();
+                }
+                else if (Objects.equals(memberRS.getString("identity"), "admin")) {
+                    Main.currentMemberName = usernameTf.getText();
+
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(new AdminHomepage(stage));
+                    stage.setScene(scene);
+                    stage.setTitle("管理员主页");
+                    stage.show();
+                    // 关闭舞台时，会弹出模态对话框确认是否退出
+                    stage.setOnCloseRequest(event1 -> {
+                        // 对话框 Alert Alert.AlertType.CONFIRMATION
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                        // 设置对话框标题
+                        alert.setTitle("退出");
+                        // 设置内容
+                        alert.setHeaderText("确定要退出吗？");
+                        // 显示对话框
+                        Optional<ButtonType> result = alert.showAndWait();
+                        // 如果点击OK
+                        if (result.get() == ButtonType.OK) {
+                            // 关闭窗体
+                            stage.close();
+                            // 否则
+                        } else {
+                            event1.consume();
+                        }
+                    });
+                    // 隐藏之前的窗体
+                    oldStage.hide();
+                }
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
