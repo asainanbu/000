037a68c44d168a500e4b12d6fa13326e603e8fc1
Tungdji <wtd53pm@outlook.com>
2021-01-02 23:18:58 +0800

    Update AdminHomepageController.java

--- src/AdminHomepageController.java
+++ src/AdminHomepageController.java
@@ -73,17 +73,98 @@ public class AdminHomepageController {
 
     @FXML
     void memberOverviewClick(ActionEvent event) {
+        // 创建新的窗体
+        Stage stage = new Stage();
+        Scene scene = new Scene(new MemberOverview(stage));
+        stage.setScene(scene);
+        stage.setTitle("会员一览");
+        stage.show();
 
+        // 关闭舞台时，会弹出模态对话框确认是否退出
+        stage.setOnCloseRequest(event1 -> {
+            // 对话框 Alert Alert.AlertType.CONFIRMATION
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            // 设置对话框标题
+            alert.setTitle("退出");
+            // 设置内容
+            alert.setHeaderText("确定要退出吗？");
+            // 显示对话框
+            Optional<ButtonType> result = alert.showAndWait();
+            // 如果点击OK
+            if (result.get() == ButtonType.OK) {
+                // 关闭窗体
+                stage.close();
+                // 否则
+            } else {
+                event1.consume();
+            }
+        });
+        // 隐藏之前的窗体
+        oldStage.hide();
     }
 
     @FXML
     void notificationClick(ActionEvent event) {
+        // 创建新的窗体
+        Stage stage = new Stage();
+        Scene scene = new Scene(new AdminNotification(stage));
+        stage.setScene(scene);
+        stage.setTitle("消息通知");
+        stage.show();
 
+        // 关闭舞台时，会弹出模态对话框确认是否退出
+        stage.setOnCloseRequest(event1 -> {
+            // 对话框 Alert Alert.AlertType.CONFIRMATION
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            // 设置对话框标题
+            alert.setTitle("退出");
+            // 设置内容
+            alert.setHeaderText("确定要退出吗？");
+            // 显示对话框
+            Optional<ButtonType> result = alert.showAndWait();
+            // 如果点击OK
+            if (result.get() == ButtonType.OK) {
+                // 关闭窗体
+                stage.close();
+                // 否则
+            } else {
+                event1.consume();
+            }
+        });
+        // 隐藏之前的窗体
+        oldStage.hide();
     }
 
     @FXML
     void reservationManagementClick(ActionEvent event) {
+        // 创建新的窗体
+        Stage stage = new Stage();
+        Scene scene = new Scene(new ReservationManagement(stage));
+        stage.setScene(scene);
+        stage.setTitle("消息通知");
+        stage.show();
 
+        // 关闭舞台时，会弹出模态对话框确认是否退出
+        stage.setOnCloseRequest(event1 -> {
+            // 对话框 Alert Alert.AlertType.CONFIRMATION
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            // 设置对话框标题
+            alert.setTitle("退出");
+            // 设置内容
+            alert.setHeaderText("确定要退出吗？");
+            // 显示对话框
+            Optional<ButtonType> result = alert.showAndWait();
+            // 如果点击OK
+            if (result.get() == ButtonType.OK) {
+                // 关闭窗体
+                stage.close();
+                // 否则
+            } else {
+                event1.consume();
+            }
+        });
+        // 隐藏之前的窗体
+        oldStage.hide();
     }
 
     public void setOldStage(Stage stage) {
@@ -99,6 +180,5 @@ public class AdminHomepageController {
         assert adminNameLabel != null : "fx:id=\"adminNameLabel\" was not injected: check your FXML file 'AdminHomepageUI.fxml'.";
 
         adminNameLabel.setText(Main.currentMemberName);
-
     }
 }
