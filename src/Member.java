419a853261ca570fcd438b9b1b440b9ebb8b21b4
Tungdji <wtd53pm@outlook.com>
2021-01-03 15:35:52 +0800

    Create Member.java

---
+++ src/Member.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+public class Member {
+
+	private String username, trueName, status;
+
+	public Member(String username, String trueName, String status) {
+		this.username = username;
+		this.trueName = trueName;
+		this.status = status;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getTrueName() {
+		return trueName;
+	}
+
+	public void setTrueName(String trueName) {
+		this.trueName = trueName;
+	}
+
+	public String getStatus() {
+		if (Objects.equals(this.status, "enabled")) {
+			return "已启用";
+		}
+		else if (Objects.equals(this.status, "disabled")) {
+			return "已禁用";
+		}
+		else if (Objects.equals(this.status, "pending")) {
+			return "待审核";
+		}
+		else {
+			return "";
+		}
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
